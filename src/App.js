07809019c68e5443f771299d1a2ecd1bbe96156c
rugieri <rugieri.ronaldo@gmail.com>
2021-04-11 19:43:24 -0300

    added players from json file

--- src/App.js
+++ src/App.js
@@ -7,21 +7,25 @@ class App extends Component {
     super();
 
     this.state = {
-      string: "Heloooooooo"
+      monsters:[]
     };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users}));
+
+  }
+
   render() {
     return (
      <div className="App">
-       <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({string:'Hello Ronaldo !!!!'})}>
-            Change Text
-          </button>     
-          
+       {this.state.monsters.map(monster => (
+           <h1 key={monster.id}> {monster.name}</h1>
+         ))}
          
-        </header>
+       
      </div>
     );
  }
